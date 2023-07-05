562f21f883051347c29163956748da67236500e9
wilfredkisku <wilfred.kisku@gmail.com>
2023-07-05 13:11:01 +0530

    model.py updated

--- MobileNetSSD300/model.py
+++ MobileNetSSD300/model.py
@@ -130,8 +130,50 @@ class VGGBase(nn.Module):
         #print(state_dict['conv7.weight'].shape)
         print('Loaded base model ...')
 
+class AuxiliaryConvolutions(nn.Module):
+    def __init__(self):
+        super(AuxiliaryConvolutions, self).__init__()
 
+        self.conv8_1 = nn.Conv2d(1024, 256, kernel_size=1, padding=0) #stride=1 
+        self.conv8_2 = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1)
+
+        self.conv9_1 = nn.Conv2d(512, 128, kernel_size=1, padding=0)
+        self.conv9_2 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)
+
+        self.conv10_1 = nn.Conv2d(256, 128, kernel_size=1, padding=0)
+        self.conv10_2 = nn.Conv2d(128, 256, kernel_size=3, padding=0)
+
+        self.conv11_1 = nn.Conv2d(256, 128, kernel_size=1, padding=0)
+        self.conv11_2 = nn.Conv2d(128, 256, kernel_size=3, padding=0)
+
+        self.init_conv2d()
+
+    def init_conv2d(self):
+        for c in self.children():
+            if isinstance(c, nn.Conv2d):
+                nn.init.xavier_uniform_(c.weight)
+                nn.init.constant_(c.bias, 0.)
+
+    def forward(self, conv7_feats):
+        
+        out = F.relu(self.conv8_1(conv7_feats))     # (N, 256, 19, 19)
+        out = F.relu(self.conv8_2(out))             # (N, 512, 10, 10)
+        conv8_2_feats = out                         # (N, 512, 10, 10)
+
+        out = F.relu(self.conv9_1(out))             # (N, 128, 10, 10)
+        out = F.relu(self.conv9_2(out))             # (N, 256, 5, 5)
+        conv9_2_feats = out                         # (N, 256, 5, 5)
+
+        out = F.relu(self.conv10_1(out))            # (N, 128, 5, 5)
+        out = F.relu(self.conv10_2(out))            # (N, 256, 3, 3)
+        conv10_2_feats = out                        # (N, 256, 3, 3)
+
+        out = F.relu(self.conv11_1(out))            # (N, 128, 3, 3)
+        conv11_2_feats = F.relu(self.conv11_2(out)) # (N, 256, 1, 1)
+
+        return conv8_2_feats, conv9_2_feats, conv10_2_feats, conv11_2_feats
 
 if __name__ == "__main__":
 
-    model = VGGBase()
+    #model = VGGBase()
+    model = AuxiliaryConvolutions()
