dbcfaf1aec3f4d9225b530a2062dec83ae180b05
wilfredkisku <wilfred.kisku@gmail.com>
2023-07-05 15:32:47 +0530

    model.py updated

--- MobileNetSSD300/model.py
+++ MobileNetSSD300/model.py
@@ -173,7 +173,75 @@ class AuxiliaryConvolutions(nn.Module):
 
         return conv8_2_feats, conv9_2_feats, conv10_2_feats, conv11_2_feats
 
+class PredictionConvolutions(nn.Module):
+
+    def __init__(self, n_classes):
+
+        super(PredictionConvolutions, self).__init__()
+
+        self.n_classes = n_classes
+
+        n_boxes = {'conv4_3':4,
+                    'conv7':6,
+                    'conv8_2':6,
+                    'conv9_2':6,
+                    'conv10_2':4,
+                    'conv11_2':4
+                }
+        #localization prediction convolutions 
+        self.loc_conv4_3 = nn.Conv2d(512, n_boxes['conv4_3']*4, kernel_size=3, padding=1)
+        self.loc_conv7 = nn.Conv2d(1024, n_boxes['conv7']*4, kernel_size=3, padding=1)
+        self.loc_conv8_2 = nn.Conv2d(512, n_boxes['conv8_2']*4, kernel_size=3, padding=1)
+        self.loc_conv9_2 = nn.Conv2d(256, n_boxes['conv9_2']*4, kernel_size=3, padding=1)
+        self.loc_conv10_2 = nn.Conv2d(256, n_boxes['conv10_2']*4, kernel_size=3, padding=1)
+        self.loc_conv11_2 = nn.Conv2d(256, n_boxes['conv11_2']*4, kernel_size=3, padding=1)
+
+        #class prediction convolutions
+        self.cl_conv4_3 = nn.Conv2d(512, n_boxes['conv4_3']*n_classes, kernel_size=3, padding=1)
+        self.cl_conv7 = nn.Conv2d(1024, n_boxes['conv7']*n_classes, kernel_size=3, padding=1)
+        self.cl_conv8_2 = nn.Conv2d(512, n_boxes['conv8_2']*n_classes, kernel_size=3, padding=1)
+        self.cl_conv9_2 = nn.Conv2d(256, n_boxes['conv9_2']*n_classes, kernel_size=3, padding=1)
+        self.cl_conv10_2 = nn.Conv2d(256, n_boxes['conv10_2']*n_classes, kernel_size=3, padding=1)
+        self.cl_conv11_2 = nn.Conv2d(256, n_boxes['conv11_2']*n_classes, kernel_size=3, padding=1)
+
+        self.init_conv2d()
+
+    def init_conv2d(self):
+        for c in self.children():
+            if isinstance(c, nn.Conv2d):
+                nn.init.xavier_uniform_(c.weight)
+                nn.init.constant_(c.bias, 0.)
+
+    def forward(self, conv4_3_feats, conv7_feats, conv8_2_feats, con9v_2_feats, conv10_2_feats, conv11_2_feats):
+
+        batch_size = conv4_3_feats.size(0)
+        
+        l_conv4_3 = self.loc_conv4_3(conv4_3_feats)
+        l_conv4_3 = l_conv4_3.permute(0, 2, 3, 1).contiguous()
+        l_conv4_3 = l_conv4_3.view(batch_size, -1, 4) 
+
+        l_conv7 = self.loc_conv7(conv7_feats)
+        l_conv7 = l_conv7.permute(0, 2, 3, 1).contiguous()
+        l_conv7 = l_conv7.view(batch_size, -1, 4)
+
+        l_conv8_2 = self.loc_conv8_2(conv8_2_feats)
+        l_conv8_2 = l_conv8_2.permute(0, 2, 3, 1).contiguous()
+        l_conv8_2 = l_conv8_2.view(batch_size, -1, 4)
+
+        l_conv9_2 = self.loc_conv9_2(conv9_2_feats)
+        l_conv9_2 = l_conv9_2.permute(0, 2, 3, 1).contiguous()
+        l_conv9_2 = l_conv9_2.view(batch_size, -1, 4)
+
+        l_conv10_2 = self.loc_conv10_2(conv10_2_feats)
+        l_conv10_2 = l_conv10_2.permute(0, 2, 3, 1).contiguous()
+        l_conv10_2 = l_conv10_2.view(batch_size, -1, 4)
+
+        l_conv11_2 = self.loc_conv11_2(conv11_2_feats)
+        l_conv11_2 = l_conv11_2.permute(0, 2, 3, 1).contiguous()
+        l_conv11_2 = l_conv11_2.view(batch_size, -1, 4)
+
 if __name__ == "__main__":
 
     #model = VGGBase()
-    model = AuxiliaryConvolutions()
+    #model = AuxiliaryConvolutions()
+    model = PredictionConvolutions(10)
