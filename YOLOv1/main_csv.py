ee427b2fe1b1c1212f5d19d9b3d7cf6dccca1176
wilfredkisku <wilfred.kisku@gmail.com>
2023-07-20 23:24:25 +0530

    restructured yolo

--- YOLOv1/main_csv.py
+++ YOLOv1/main_csv.py
@@ -2,8 +2,13 @@ import os
 import csv
 import pandas as pd
 
+def split(df, train_pre):
+    return df[:int(len(df)*train_pre)], df[int(len(df)*train_pre):]
+
 def create_csv(path):
     
+    assert len(os.listdir(path)) != 0, "LABEL text files should be generated."
+
     lst_file = os.listdir(path)
     my_dict = {}
     lst_images = [val[:-3]+'jgp' for val in lst_file]
@@ -20,8 +25,13 @@ def create_csv(path):
     print(my_df.head())
     my_df.to_csv('mycsvfile_train.csv',index=False, header=False)
     #my_df.to_csv('mycsvfile_train.csv',index=False)
-    return None
+    return my_df
 
 if __name__ == "__main__":
 
-    create_csv('labels_persons/')
+    my_df = create_csv('labels_persons/')
+    my_df_train, my_df_test = split(my_df, 0.7)
+
+    print(len(my_df_train), len(my_df_test))
+    my_df_train.to_csv('mycsvfile_train_split.csv',index=False, header=False)
+    my_df_test.to_csv('mycsvfile_test_split.csv',index=False, header=False)
