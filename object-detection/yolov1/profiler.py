1fc9bdf62218c56282c88655feee9ce3b78f53ad
Wilfred Kisku <wilfred.kisku@gmail.com>
2023-12-11 20:37:40 +0530

    Add files via upload

---
+++ object-detection/yolov1/profiler.py
@@ -0,0 +1,49 @@
+import torch
+from torch import nn
+from torchprofile import profile_macs
+
+
+def get_model_macs(model, inputs) -> int:
+    return profile_macs(model, inputs)
+
+
+def get_sparsity(tensor: torch.Tensor) -> float:
+    """
+    calculate the sparsity of the given tensor
+        sparsity = #zeros / #elements = 1 - #nonzeros / #elements
+    """
+    return 1 - float(tensor.count_nonzero()) / tensor.numel()
+
+
+def get_model_sparsity(model: nn.Module) -> float:
+    """
+    calculate the sparsity of the given model
+        sparsity = #zeros / #elements = 1 - #nonzeros / #elements
+    """
+    num_nonzeros, num_elements = 0, 0
+    for param in model.parameters():
+        num_nonzeros += param.count_nonzero()
+        num_elements += param.numel()
+    return 1 - float(num_nonzeros) / num_elements
+
+def get_num_parameters(model: nn.Module, count_nonzero_only=False) -> int:
+    """
+    calculate the total number of parameters of model
+    :param count_nonzero_only: only count nonzero weights
+    """
+    num_counted_elements = 0
+    for param in model.parameters():
+        if count_nonzero_only:
+            num_counted_elements += param.count_nonzero()
+        else:
+            num_counted_elements += param.numel()
+    return num_counted_elements
+
+
+def get_model_size(model: nn.Module, data_width=32, count_nonzero_only=False) -> int:
+    """
+    calculate the model size in bits
+    :param data_width: #bits per element
+    :param count_nonzero_only: only count nonzero weights
+    """
+    return get_num_parameters(model, count_nonzero_only) * data_width
